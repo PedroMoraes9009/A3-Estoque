7eb271fe36980c2b08d73a43a99e2e381625eeb2
cauac <cauac@192.168.0.180>
2025-05-29 16:27:19 -0300

    Merge origin/main into main

--- src/main/java/com/mycompany/estoque/Estoque.java
+++ src/main/java/com/mycompany/estoque/Estoque.java
@@ -4,15 +4,20 @@
 
 package com.mycompany.estoque;
 
+
 /**
  *
  * @author Pedro Moraes
  */
-import view.MenuView;
+//import view.MenuView;
+import View.FrmMenu;
 public class Estoque {
 
    public static void main(String[] args) {
-        MenuView menu = new MenuView();
-        menu.exibirMenu();
-    }
+//        MenuView menu = new MenuView();
+//        menu.exibirMenu();
+        
+        FrmMenu menuEstoque = new FrmMenu();
+        menuEstoque.setVisible(true);
+   }
 }
